58f6fc80e1c6f33cbcb160f0910211901b1ca2ed
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2023-12-20 17:59:51 +0500

    Add project files.

---
+++ C#_9-dars_OOP Polymorphism and etc/9-dars_hotel/Person.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace C__9_dars_OOP_Polymorphism_and_etc._9_dars_hotel
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string SSN { get; set; }
+
+        public Person(string name, string surname, string ssn)
+        {
+            Name = name;
+            Surname = surname;
+            SSN = ssn;
+
+        }
+
+        public ArrayList bemorlar = new ArrayList();
+
+        // Bemorni qo'shish
+        public void addPatient(Person person)
+        {
+            //var bemor = new Person(ismi, familiyasi, ssn);
+            bemorlar.Add(person);
+            Console.WriteLine("Bemor muvaffaqiyatli qo'shildi");
+            foreach(Person i in person.bemorlar)
+{
+                Console.Write(i.Name+ " ,");
+                Console.Write(i.Surname + ", ");
+                Console.Write(i.SSN);
+            }
+        }
+    }
+
+
+}
---
+++ C#_9-dars_OOP Polymorphism and etc/Program.cs
@@ -0,0 +1,13 @@
+﻿using C__9_dars_OOP_Polymorphism_and_etc._9_dars_hotel;
+
+#region Calculator
+//Hisobla<int> oyla = new Hisobla<int>();
+//Console.WriteLine(oyla.Qoshish(2, 4));
+//Console.WriteLine(oyla.Bolish(4, 2));
+//Console.WriteLine(oyla.Kopaytirish(4, 2));
+//Console.WriteLine(oyla.Ayirish(4, 2));
+#endregion
+
+Person person = new Person("Sotqinjon", "Polvonov", "1");
+person.addPatient(person);
+
